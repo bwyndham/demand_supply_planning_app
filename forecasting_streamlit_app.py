5c1dde3a86daddb0ec97f3553af2e0f462b96bab
Ben Wyndham <bwyndham@utexas.edu>
2024-11-18 16:46:51 -0600

    more caching, new data

--- forecasting_streamlit_app.py
+++ forecasting_streamlit_app.py
@@ -19,6 +19,12 @@ def read_from_gsheet_to_df(filename):
      gc = gspread.service_account_from_dict(creds)
      sh = gc.open(filename)
      df = pd.DataFrame(sh.sheet1.get_all_records())
+     df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+     if 'ds' in df.columns:
+          df['ds']  = pd.to_datetime(df['ds']).dt.date
+     else:
+          pass
+     
      #st.toast(f"{filename} data loaded!")
      return df 
 
@@ -29,14 +35,17 @@ def write_to_gsheet(filename, data):
      sh.sheet1.append_row(data, table_range="A1:D1")
 
 st.title("Demand and Supply Planning App")
-mkdwn = '''Welcome to the Demand and Supply Planning App.  
+mkdwn = '''Welcome to the Demand and Supply Planning App, an educational project by Ben Wyndham.
 
-The purpose of this app is to provide automatic, high-quality, hierarchical demand forecasting and supply planning via a simple interface. This app uses the Nixtla, Streamlit, Pandas, Gspread and Plotly Python libraries. 
-Using Nixtla's efficient algorithms and multi-threading, this app forecasts 50k series from the M5 dataset in minutes on a machine with 16GB of memory and 12 CPU cores.  
+The purpose of this app is to provide automatic, high-quality, hierarchical demand forecasting and supply planning via a simple interface. This app is purely educational, and assumptions or notes are listed throughout the app. 
+
+The forecasts are driven by an ML and Statistical forecaster that selects the best model for each time series, based on crossvalidation performance across 10, 6 week folds. This forecaster uses a wide selection of models such as LightGBM, Croston, HoltWinters, and ETS. Code can be found on [github](https://github.com/bwyndham/demand_supply_planning_app/blob/main/ml-forecaster.ipynb).
+
+This app uses the Nixtla, LightGBM, Streamlit, Pandas, Gspread and Plotly Python libraries.  
 
 If you have any feedback or suggestions, please let me know via the feedback form or my [website](https://benjaminwyndham.com/).'''
 st.markdown(mkdwn)
-tab1, tab2, tab3, tab4 = st.tabs(['Data Import','Demand Planning','Supply Planning','Feedback'])
+tab1, tab2, tab3 = st.tabs(['Demand Planning','Supply Planning','Feedback'])
 # hardcoded, need to refactor to get user input and save in this format
 categorial_columns = ["state_id","store_id","cat_id","dept_id"]
 item_column = 'item_id'
@@ -46,29 +55,21 @@ historic_column = 'y'
 date_column = ['ds']
 
 df = read_from_gsheet_to_df("forecasted_values")
-# get historical data
-hist_df = read_from_gsheet_to_df("training_data_nonhierarchical")
-hist_df['ds'] = pd.to_datetime(hist_df['ds'])
 
-# combine historical and forecast data
-ttl_df = pd.concat([df, hist_df])
-ttl_df['ds']  = pd.to_datetime(ttl_df['ds']).dt.date
-ttl_df.drop(columns=['unique_id'],inplace=True)
+
+ttl_df = read_from_gsheet_to_df("combined_sales_fcast")
+#ttl_df['ds']  = pd.to_datetime(ttl_df['ds']).dt.date
 ttl_df.sort_values(by=date_column, inplace=True)
+
+# get historical data
+hist_df = ttl_df[ttl_df['forecast'].isna()]
+
 # get error metrics
 error_df = read_from_gsheet_to_df("hierarchical_evaluations_summary")
-
-with tab1:
-     upload_sales = st.button("Upload Sales Data", key="sales_data_button")
-     upload_inventory = st.button("Upload Inventory Data", key="inv_data_button")
-     if upload_sales:
-          st.info("Thank you for your interest in this app! Right now, the data import and forecast functionality are not available. If you would like a demo of the forecast functionality, please reach out to me at my [website](https://benjaminwyndham.com/).")
-     if upload_inventory:
-          st.info("Thank you for your interest in this app! Right now, the data import and forecast functionality are not available. If you would like a demo of the forecast functionality, please reach out to me at my [website](https://benjaminwyndham.com/).")
-     # show the dataframes, configure columns
+best_models = read_from_gsheet_to_df("forecast_model_breakout")
      
-with tab2:
-     start_date = st.date_input("Start Date of Historical Data", dt.datetime(2011,1,1))
+with tab1:
+     start_date = st.date_input("Start Date of Historical Data", dt.datetime(2015,10,1))
      groups = ttl_df[categorial_columns].columns.to_list()
      group_choices = []
      group_choice = st.selectbox('Select Group By',groups)
@@ -96,41 +97,47 @@ with tab2:
      with col1:
           ttl_fcast = ttl_df[ind][forecast_column].sum()
           st.metric(label="Forecasted Total Units", value="{:,.0f} units".format(ttl_fcast))
+          
+          try:
+               # historical line chart
+               fig = px.line(ttl_df[ind & (ttl_df['ds'] > start_date) & (~ttl_df[historic_column].isna())].groupby(by=time_groupby).agg({historic_column:'sum'}).reset_index(),
+                         x=date_column, y=historic_column, color=group_choice, title='Historical Sales',range_y=[0,150000])
+               st.plotly_chart(fig, key='historic_series')
+          except Exception as e:
+               st.exception(e)
+               
      with col2:
           ttl_error = error_df[error_df['level'] == 'Overall']['forecast'].values[0]
           error_metric = error_df[error_df['level'] == 'Overall']['metric'].values[0]
-          st.metric(label=f"Forecast Overall Error ({error_metric})",value = "{:,.0f} square units".format(ttl_error))
-     
-     try:
+          st.metric(label=f"Forecast Average Error ({error_metric})",value = "{:,.0f} units".format(ttl_error))
+          
+          try:
           # forecast line chart
-          fig = px.line(ttl_df[ind & (~ttl_df[forecast_column].isna())].groupby(by=time_groupby).agg({forecast_column:'sum'}).reset_index(),
-                        x=date_column, y=forecast_column, color=group_choice, title='Forecasted Sales')
-          st.plotly_chart(fig, key='forecast_test')
-     except Exception as e:
-          st.exception(e)
-     try:
-          # historical line chart
-          fig = px.line(ttl_df[ind & (ttl_df['ds'] > start_date) & (~ttl_df[historic_column].isna())].groupby(by=time_groupby).agg({historic_column:'sum'}).reset_index(),
-                    x=date_column, y=historic_column, color=group_choice, title='Historical Sales')
-          st.plotly_chart(fig, key='historic_series')
-     except Exception as e:
-          st.exception(e)
+               fig = px.line(ttl_df[ind & (~ttl_df[forecast_column].isna())].groupby(by=time_groupby).agg({forecast_column:'sum'}).reset_index(),
+                        x=date_column, y=forecast_column, color=group_choice, title='Forecasted Demand',range_y=[0,150000])
+               st.plotly_chart(fig, key='forecast_test')
+          except Exception as e:
+               st.exception(e)
+          
      
      st.info("Filter to a department and select up to 10 Items Below to view their forecasts.")
      depts = ttl_df['dept_id'].drop_duplicates()
      dept_choice = st.selectbox('Select Dept_Id',depts)
           
-     test_df = pd.pivot_table(ttl_df[~ttl_df[forecast_column].isna() & (ttl_df['dept_id'] == dept_choice)], values=forecast_column,
+     item_df = pd.pivot_table(ttl_df[~ttl_df[forecast_column].isna() & (ttl_df['dept_id'] == dept_choice)], values=forecast_column,
                               index=cat_item_columns, columns=date_column, aggfunc='sum', fill_value=0)
      
-     test_df['series'] = test_df.iloc[:,-4:].values.tolist()
-     #test_df = test_df.iloc[:,-1:]
+     item_df['series'] = item_df.iloc[:,-6:].values.tolist()
      column_configuration = {'series': st.column_config.LineChartColumn("Forecast")}
-     event = st.dataframe(test_df,use_container_width=True,hide_index=False,on_select='rerun',selection_mode='multi-row',column_config=column_configuration)
+     event = st.dataframe(item_df,use_container_width=True,hide_index=False,on_select='rerun',selection_mode='multi-row',column_config=column_configuration)
      selections = event.selection.rows
-     filtered_df = test_df.iloc[selections].reset_index().melt(id_vars=cat_item_columns)
+     filtered_df = item_df.iloc[selections].reset_index().melt(id_vars=cat_item_columns)
      fig = px.line(filtered_df,x='ds', y='value', color='item_id')
      st.plotly_chart(fig, key='filtered_items_series')
+     
+     st.info("Breakout of Forecasts and Quantity by Forecasting Model")
+     st.warning("There are clear opportunities within the forecaster to improve results. With the right data and parameters, many of these algorithms can and should beat benchmarks like Naive and Average models.")
+     st.dataframe(best_models)
      
      forecasts_csv = df.to_csv()
      st.download_button(label="Download Forecasts to CSV",
@@ -138,11 +145,12 @@ with tab2:
                         file_name="forecasts.csv",
                         mime="text/csv")
 
-with tab3:
+with tab2:
      oh_df= read_from_gsheet_to_df("on_hand_supply_planning")
      demand_df = df.groupby(by='unique_id').sum(forecast_column).reset_index()
      compare = supply_calculation(oh_df, demand_df)
      hist_start = hist_df['ds'].max() - dt.timedelta(weeks=4)
+     st.warning("For simplicity, 0 week lead time is assumed. In practice that is rarely, if ever, the case.")
      
      groups = ttl_df[categorial_columns].columns.to_list()
      group_choices = []
@@ -173,17 +181,17 @@ with tab3:
      
      avg_sales_4wk = (hist_df[hist_ind & (hist_df['ds'] >= hist_start)]['y'].sum())/4
      curr_oh = compare[compare_ind]['On Hand'].sum()
-     wos = curr_oh / avg_sales_4wk
+     woc = curr_oh / avg_sales_4wk
      fcast_sales_4wk = (compare[compare_ind][forecast_column].sum()/4)
      fwos = curr_oh / fcast_sales_4wk
      ttl_buy = compare[compare_ind]['Quantity to Meet Demand'].sum()
      col1, col2, col3 = st.columns(3)
      with col1:
-          st.metric(label="Quantity to Meet Forecasted Demand", value="{:,.0f} units".format(ttl_buy))
+          st.metric(label="Quantity to Meet Forecasted Demand", value="{:,.0f} qty".format(ttl_buy))
      with col2:
-          st.metric(label="Weeks of Supply (L4W)", value="{:,.0f} weeks".format(wos))
+          st.metric(label="Weeks of Cover (L4W)", value="{:,.0f} weeks".format(woc))
      with col3:
-          st.metric(label="Weeks of Supply (Forecasted Next 4W)", value="{:,.0f} weeks".format(fwos))
+          st.metric(label="Weeks of Supply (Forecasted Next 4)", value="{:,.0f} weeks".format(fwos))
      
      fig = px.bar(compare[compare_ind].groupby(by=group_choice).agg({'Quantity to Meet Demand':'sum'}).reset_index(),x=group_choice,y='Quantity to Meet Demand')
      st.plotly_chart(fig, key='agg_buys')
@@ -195,7 +203,7 @@ with tab3:
                         file_name="supply_recommendations.csv",
                         mime="text/csv")
 
-with tab4:
+with tab3:
      with st.form("feedback_form"):
           st.write("What do you think about this app?")
           star_rating = st.feedback("stars")
